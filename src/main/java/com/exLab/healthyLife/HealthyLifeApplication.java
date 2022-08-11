cefc1b67d81809e795dedf9e4bfa7c5f9daa6e0b
Polina Arkhipenko <polinar977@gmail.com>
2022-08-11 21:55:24 +0300

    Initial commit

---
+++ src/main/java/com/exLab/healthyLife/HealthyLifeApplication.java
@@ -0,0 +1,13 @@
+package com.exLab.healthyLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyLifeApplication.class, args);
+	}
+
+}
