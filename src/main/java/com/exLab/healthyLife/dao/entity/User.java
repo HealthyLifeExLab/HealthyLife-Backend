c57250fa53c57124e26b4dfdcf17af6b3a9cd334
Polina Arkhipenko <polinar977@gmail.com>
2022-09-06 20:37:34 +0300

    fix recurring email in DB

--- src/main/java/com/exLab/healthyLife/dao/entity/User.java
+++ src/main/java/com/exLab/healthyLife/dao/entity/User.java
@@ -17,7 +17,7 @@ public class User {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "password", nullable = false)
